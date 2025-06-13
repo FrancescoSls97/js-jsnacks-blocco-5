97c8dc6d11d2f801f8d7a1adbc4c15c64c45f4ef
FrancescoSls97 <frankds1997@gmail.com>
2025-06-13 15:10:49 +0200

    complete snack 2

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -1,8 +1,14 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const peoplesName = [];
+people.forEach((name, i, arr) => {
+  peoplesName.push(name);
+});
+console.log(peoplesName);
